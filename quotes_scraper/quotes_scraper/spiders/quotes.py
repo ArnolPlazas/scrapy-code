03082986887e3f7ab4a6b0cf67180f0189c62e9a
Arnol Plazas <plazasarnol@gmail.com>
2021-08-18 17:51:41 -0500

    Usando XPath para extraer datos

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,14 +1,35 @@
 import scrapy
 
+# Titulo = //h1/a/text()
+# Citas = //span[@class = "text" and @itemprop="text"]/text()
+# Top ten tags = //div[contains(@class, "tags-box"]//span[@class="tag-item"]/a/text()
+
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls =[
         'http://quotes.toscrape.com/'
     ]
-    # analizar la respuesta http que nos envia la peticion de esta pagina y xon ella extrar el la información       
+    # analizar la respuesta http que nos envia la peticion de esta pagina y con ella extraer la información       
     def parse(self, response):
         print('*' * 10)
-        print(response.status, response.headers)
+        print('\n\n')
+        #print(response.status, response.headers)
+        
+        title = response.xpath('//h1/a/text()').get()
+        print(f'Titulo: {title}')
+        print('\n\n')
+
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        for quote in quotes:
+            print(f'Cita: {quote}')
+        print('\n\n')
+
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
+        for tag in top_ten_tags:
+            print(f'Tag: {tag}')
+        print('\n\n')
+
         print('*' * 10)
+        print('\n\n')
 
         
