471710563738893710bcfab1a0d3fa2ef2ec1ed9
Arnol Plazas <plazasarnol@gmail.com>
2021-08-19 17:34:42 -0500

    Guardar los datos

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -11,25 +11,15 @@ class QuotesSpider(scrapy.Spider):
     ]
     # analizar la respuesta http que nos envia la peticion de esta pagina y con ella extraer la información       
     def parse(self, response):
-        print('*' * 10)
-        print('\n\n')
-        #print(response.status, response.headers)
-        
         title = response.xpath('//h1/a/text()').get()
-        print(f'Titulo: {title}')
-        print('\n\n')
-
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        for quote in quotes:
-            print(f'Cita: {quote}')
-        print('\n\n')
-
         top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
-        for tag in top_ten_tags:
-            print(f'Tag: {tag}')
-        print('\n\n')
+        
+        yield {
+            'Title': title,
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+        } 
 
-        print('*' * 10)
-        print('\n\n')
 
         
